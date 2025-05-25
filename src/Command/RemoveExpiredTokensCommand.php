f73fff6d4db39c633e63657aab42bcfe5e1e6290
SaHair529 <sa.hairulaev@gmail.com>
2025-05-25 07:44:27 +0300

    Добавил команду для удаления истекших токенов и связанных с ними файлов

---
+++ src/Command/RemoveExpiredTokensCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Token;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+#[AsCommand(
+    name: 'app:remove-expired-tokens',
+    description: '',
+)]
+class RemoveExpiredTokensCommand extends Command
+{
+    public function __construct(private EntityManagerInterface $em, private KernelInterface $kernel)
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $expiredTokens = $this->em->getRepository(Token::class)
+            ->createQueryBuilder('t')
+            ->where('t.expires_in < :now')
+            ->setParameter('now', new \DateTimeImmutable())
+            ->getQuery()
+            ->getResult();
+            
+        $filesystem = new Filesystem();
+        
+        foreach ($expiredTokens as $token) {
+            $uploadDir = $this->kernel->getProjectDir() . '/public/uploads/' . $token->getValue();
+            if ($filesystem->exists($uploadDir)) {
+                $filesystem->remove($uploadDir);
+            }
+            $this->em->remove($token);
+        }
+        $this->em->flush();
+
+        $output->writeln('Expired files removed successfully.');
+
+        return Command::SUCCESS;
+    }
+}
